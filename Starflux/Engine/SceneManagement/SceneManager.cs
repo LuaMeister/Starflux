e9f1e75328f0d246fe4e3f94dc9a640783804752
Ryan Hollink <ryan.hollink@hotmail.com>
2023-02-16 18:14:01 +0100

    marked SceneManager as static and added some documentation

--- Starflux/Engine/SceneManagement/SceneManager.cs
+++ Starflux/Engine/SceneManagement/SceneManager.cs
@@ -7,7 +7,7 @@ using Microsoft.Xna.Framework.Content;
 
 namespace StarfluxEngine.SceneManagement;
 
-public class SceneManager
+public static class SceneManager
 {
 	#region Public Fields
 
@@ -24,24 +24,36 @@ public class SceneManager
 	
 	#region Public Methods
 
+	/// <summary>
+	/// Unloads the previous scene and loads the given scene.
+	/// </summary>
+	/// <param name="sceneNumber">The build index of the scene to load</param>
 	public static void LoadScene(int sceneNumber)
 	{
 		var sceneName = GetSceneName(sceneNumber);
 		var documentPath = GetDocumentPath(sceneName);
 		var sceneDocument = LoadSceneDocument(documentPath);
-		Console.WriteLine(sceneDocument.DocumentElement);
 	}
 
 	#endregion
 
 	#region Private Methods
-
+	
+	/// <summary>
+	/// Gets the path to the document where this scene should be at
+	/// </summary>
+	/// <param name="sceneName">Name of the scene we need to find</param>
+	/// <returns>The path to the scene document</returns>
 	private static string GetDocumentPath(string sceneName)
 	{
-		Console.WriteLine(ContentManager.RootDirectory);
 		return Path.Combine(ContentManager.RootDirectory, "Scenes", sceneName + ".xml");
 	}
 
+	/// <summary>
+	/// Creates and loads the XML Document
+	/// </summary>
+	/// <param name="documentPath">Path to the document of the scene</param>
+	/// <returns>An XML Document that holds information about the to load</returns>
 	private static XmlDocument LoadSceneDocument(string documentPath)
 	{
 		XmlDocument sceneDocument = new();
@@ -50,6 +62,11 @@ public class SceneManager
 		return sceneDocument;
 	}
 
+	/// <summary>
+	/// Checks the BuildIndex for a name that exists at that number
+	/// </summary>
+	/// <param name="sceneNumber">The number that will be used as the index</param>
+	/// <returns>A string with the name of this scene, or an empty string if this index did not have a name</returns>
 	private static string GetSceneName(int sceneNumber)
 	{
 		string sceneName = BuildIndex[sceneNumber];
